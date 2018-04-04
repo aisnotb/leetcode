65d36dfc904c963419e1ecc62d8f7c1044b06a03
huageng <unstoppablediaosi@gmail.com>
2018-04-04 23:49:18 +0800

    增加了投票算法的英文说明书

---
+++ 169_majority_element.java
@@ -0,0 +1,45 @@
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+
+
+我的思路
+
+这个题一个简单的想法就是 去直接数 每一个元素出现了多少次， 然后直接得出次数最多的那个元素
+于是我们需要一个 hashmap， 把一个元素和他对应的次数 记录下来。
+
+
+第一种方法 暴力解法
+class Solution {
+    public int majorityElement(int[] nums) {
+
+    	int majority = nums.length / 2;
+		
+		for (int i = 0 ; i < nums.length ; i++) {
+			int count = 0;
+			for (int j = 0 ; j < nums.length; j++ ) {
+				if (nums[i] == nums[j]) {
+					count += 1;
+		  		}      		
+		  		if (count > majority) {
+					return nums[i];
+		  		}
+			}      	
+		}      
+
+		return -1;
+    }
+}
+这种解法 有个小技巧 就是 只用了一个变量来保存一个元素 出现的次数， 使得空间复杂度降到了 最低。 
+
+class Solution{
+	public int majorityElement(int[] nums){
+		
+	}
+}
+
+
+
