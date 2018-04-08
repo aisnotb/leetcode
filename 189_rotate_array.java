8d3e863ac379b52adedb05f1e4721f9a586543f4
huageng <unstoppablediaosi@gmail.com>
2018-04-08 18:19:33 +0800

    todo

--- 189_rotate_array.java
+++ 189_rotate_array.java
@@ -30,20 +30,47 @@ class Solution {
 	public void rotate(int[] nums, int k){
 		int n = nums.length;
 		for (int i = 0 ; i < k; i++) {
-			int cur = nums[n-1];
-
-			for (int j = 0; j < n ; j++) {
-
+			var temp = nums[n-1]; //    我们需要保存数组的最后一个元素 
+			for (int j = n - 1; j > 0 ; j--) {
+				nums[j] = nums[j-1];
 			}
+			nums[0] = temp;
 		}
 	}
 }
+很高兴自己写的这个例子一次通过了。但是也发现了很多不足
+时间 复杂度 n 平方
+空间 复杂度 o（1）
 
 
 解法 2 使用额外的数组来解决这个问题
 
+一般来解决空间复杂度 太高的方法都是用 空间换时间。是不是现在的电脑 内存要求越来越高的原因啊  ：(
+根据我们的观察，我们可以直接把 这个数组的后面 k 个元素 放到 新数组的前面，然后把 前面的元素放到数组的后面
+
 class Solution {
     public void rotate(int[] nums, int k) {
-   
+    	int n = nums.length;
+    	k = k % n;
+		int[] arr = new Int[n];
+		for (int i = 0; i < n;  i++) {
+			arr[i + k] = nums[i];
+		}
+		for (int i = 0;  i < n;  i++) {
+			nums[i] = arr[i];
+		}
+		
     }
 }
+
+这个是错误的做法。应为这个函数返回值是 void。 所以我们只能在该数组上面下手。
+
+
+以后再做
+解法 3 这个题的确可以 一次 解决，应为对于一个给定 长度 和 k 的数组， 我们可以通过计算 来直接得到新的数组。
+class Solution{
+	public void rotate(int[] nums, int k){
+
+	}
+}
+
