90721c3e5e28b190efb4adddde674659599ebe79
huageng <unstoppablediaosi@gmail.com>
2018-04-11 18:42:56 +0800

    todo later 我在用--amend 选项

---
+++ 153_find_minimum_in_rotated_sorted_array.java
@@ -0,0 +1,20 @@
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+
+我的思路
+首先 暴力解法。。太无聊了。省略吧 。
+
+然后想到的是 
+不旋转的时候 最小的元素 就是左边的第一个元素 。
+
+旋转之后最大的元素和最小的元素是紧紧靠在一起的。但是这对解决这个问题没有任何帮助。
+
+
+
+
