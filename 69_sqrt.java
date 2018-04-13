b85abe3d05670d69af1664bb9b22284fbc2b4522
carson <stehliu25@gmail.com>
2018-04-13 23:15:26 +0800

    修改了sqrt的解法

--- 69_sqrt.java
+++ 69_sqrt.java
@@ -37,6 +37,28 @@ class Solution{
 	    }
 	}
 }
+今天我又刷了一遍。但是又出问题了。
+
+这是我重新的提交
+class SOlution{
+	public int mySqrt(int x){
+		if(x == 0) return 0;
+		int l = 1;
+		int r = x; 
+		while(l <= r){
+			int mid = l + (r - l) / 2;
+			if( mid == x / mid) {
+				return mid;
+			}else if( mid > x / mid) {
+				r = mid - 1;	
+			}else if( mid < x / mid ){
+				l = mid + 1;
+			}
+		}
+		return r;
+	}
+
+}
 
 这个题目有两个地方要注意 。一个是 
 
