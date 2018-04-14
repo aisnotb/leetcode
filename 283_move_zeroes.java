6fb3ef3c83d888dfeec84e8d456ca22e62e1d989
carson <stehliu25@gmail.com>
2018-04-14 11:16:45 +0800

    第二次对move zero 的编写

--- 283_move_zeroes.java
+++ 283_move_zeroes.java
@@ -61,3 +61,20 @@ class Solution{
 	}
 }
 
+第二次尝试写这个算法
+class Solution{
+	public void moveZeroes(int nums[]){
+		int n = nums.length;
+		int i = 0;
+		int count = 0;
+		for(int j = 0 ; j < nums.length; j++){
+			if(nums[j] != 0 ){
+				nums[i++] = nums[j];
+				count++;
+			}
+		}
+		for(int m = count; m < n; m++){
+			nums[m] = 0;
+		}
+	}
+}
