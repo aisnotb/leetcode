5789c2b269447ac94c8f6fa1658745da05786e9d
carson <stehliu25@gmail.com>
2018-04-16 21:28:37 +0800

    写出来haystack

--- 28_implement_strStr.java
+++ 28_implement_strStr.java
@@ -18,27 +18,34 @@ What should we return when needle is an empty string? This is a great question t
 
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 
+这个题我原来的想法是用两个指针从头开始扫描
+但是算法没有写出来，觉得好尴尬。
+
+后来我又想到了用两个只需要比较   
 
 class Solution{
 	public int strStr(String haystack, String needle){
-		int n1 = haystack.length;
-		int n2 = needle.length;
+		int n1 = haystack.length();
+		int n2 = needle.length();
+		if(n2 == 0) return 0;
 		if(n2 > n1 ) return -1;
-		int i = 0 ;
-		int j = 0 ;
-		int begin = -1;
-		while(i< n1 && j< n2){
-				while(needle.indexOf(j)!=haystack.indexOf(i)){
-					i++;
-					begin = i;
-				}
-				j++;
-				while(needle.indexOf(j) == haystack.indexOf(i)){
-					i++;
-					j++;
-				}
+		for(int i = 0 ; i < l1-l2+1; i++){
+			int count = 0;
+			while(count < n2 && haystack.charAt(i+count) === needle.charAt(count)){
+			count++;
 			}
+			if(count == n2 ){
+				return i;
+			}
+
 		}
 		return -1;
 	}
 }
+
+总结
+首先字符串的长度 要用s.length() 这个函数来获得
+
+其次需要考虑其中一个字符串为空的情况
+
+
